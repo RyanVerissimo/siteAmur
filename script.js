032f07890d6e4ba3322a37e4e08708703d7fa3c0
RyanVerissimo <ryanvm.black@gmail.com>
2025-04-01 20:06:35 -0300

    atualizando

--- script.js
+++ script.js
@@ -47,23 +47,31 @@ document.addEventListener("DOMContentLoaded", function() {
 
     function updateCounter() {
         const startDate = new Date("2025-02-08T00:00:00");
-        setInterval(() => {
+    
+        function updateTime() {
             const now = new Date();
             let diff = now - startDate;
+    
             const years = Math.floor(diff / (1000 * 60 * 60 * 24 * 365.25));
             diff -= years * (1000 * 60 * 60 * 24 * 365.25);
             const months = Math.floor(diff / (1000 * 60 * 60 * 24 * 30.44));
             diff -= months * (1000 * 60 * 60 * 24 * 30.44);
-            const days = Math.round(diff / (1000 * 60 * 60 * 24));
+            const days = Math.floor(diff / (1000 * 60 * 60 * 24));
             diff -= days * (1000 * 60 * 60 * 24);
             const hours = Math.floor(diff / (1000 * 60 * 60));
             diff -= hours * (1000 * 60 * 60);
             const minutes = Math.floor(diff / (1000 * 60));
             diff -= minutes * (1000 * 60);
             const seconds = Math.floor(diff / 1000);
-            document.getElementById("time-count").textContent = `${years} anos, ${months} meses, ${days} dias, ${hours} horas, ${minutes} minutos e ${seconds} segundos`;
-        }, 1000);
+    
+            document.getElementById("time-count").textContent = 
+                `${years} anos, ${months} meses, ${days} dias, ${hours} horas, ${minutes} minutos e ${seconds} segundos`;
+        }
+    
+        updateTime();
+        setInterval(updateTime, 1000);
     }
+    
     updateCounter();
 
     function createParticles() {
@@ -87,9 +95,4 @@ document.addEventListener("DOMContentLoaded", function() {
     }
     
     createParticles();
-    
-    
-    
-    
-    
 });
